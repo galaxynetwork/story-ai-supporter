4659bb1539eb0289c9f6ce6c380b1140055109b7
GLXwhitetiger <glxverse@gmail.com>
2022-05-20 13:04:01 +0900

    refactor: flask => fastapi

---
+++ app.py
@@ -0,0 +1,91 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from fastapi.encoders import jsonable_encoder
+
+import boto3
+import json
+
+app = FastAPI()
+
+# Amazon SageMaker Runtime Client
+session = boto3.Session()
+sagemaker_runtime = session.client('sagemaker-runtime')
+
+# Sagemaker endpoint name.
+endpoint_name = 'sm-endpoint-gpt-j-6b'
+
+
+class DataIn(BaseModel):
+    text: str
+    datas: dict
+
+
+@app.get("/elb")
+async def server_status():
+    response_body = {
+        'status': 200,
+        'data': "Healthy"
+    }
+
+    return jsonable_encoder(response_body)
+
+
+@app.post('/gen-text')
+def text_generate(ai_params: DataIn):
+    response_body = {
+        'status': 100,
+        'data': ""
+    }
+
+    payload = {
+        'inputs': "",
+        'parameters': {
+            'max_length': 100,
+            'do_sample': True,
+            'no_repeat_ngram_size': 2,
+            'temperature': 0.75,
+            'top_k': 10,
+            'top_p': 0.95,
+            'early_stopping': True,
+        }
+    }
+
+    user_text = ai_params.dict()['text']
+    if user_text == "":
+        response_body['status'] = 400
+        response_body['data'] = "Please enter text."
+
+        return jsonable_encoder(response_body)
+
+    tempr_param = float(ai_params.dict()['datas']['randomness'])
+    if tempr_param < 0.01:
+        tempr_param = 0.01
+
+    top_k_param = int(ai_params.dict()['datas']['fluency'])
+    if top_k_param < 1:
+        top_k_param = 1
+
+    payload['inputs'] = user_text
+    payload['parameters']['temperature'] = tempr_param
+    payload['parameters']['top_k'] = top_k_param
+
+    try:
+        response = sagemaker_runtime.invoke_endpoint(
+            EndpointName=endpoint_name,
+            ContentType='application/json',
+            Body=json.dumps(payload)
+        )
+        result = json.loads(response['Body'].read().decode())
+
+        raw_text = result[0]['generated_text']
+        res_text = str(raw_text).replace(user_text, "").replace("\n", " ").replace('"', "")
+
+        response_body['status'] = 200
+        response_body['data'] = res_text
+
+        return jsonable_encoder(response_body)
+    except:
+        response_body['status'] = 503
+        response_body['data'] = "AI server is overloaded"
+
+        return jsonable_encoder(response_body)
